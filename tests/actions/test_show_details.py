f54060e94939b1b308093f152adfd9e503d617b4
Rafaela Plinta <ra.plinta@gmail.com>
2023-11-22 14:40:23 -0300

    Add tests show_details

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,1 +1,70 @@
-from pro_filer.actions.main_actions import show_details  # NOQA
+from pro_filer.actions.main_actions import show_details
+import os  # o módulo os interage com o sistema operacional
+# https://docs.python.org/3/library/os.path.html
+from datetime import datetime
+
+
+# teste para quando o arquivo não existe
+def test_show_details_file_not_exists(capsys):
+    mock = {"base_path": "/nonexistent_file.txt"}
+    show_details(mock)
+    captured = capsys.readouterr()
+    assert captured.out == "File 'nonexistent_file.txt' does not exist\n"
+
+
+# teste para quando o arquivo existe e está tudo certo
+def test_show_details_all_info(capsys, tmp_path):
+    # aqui crio um file temorário no meu diretório temporário
+    tmp_file_path = tmp_path / "my_file.txt"
+    with open(tmp_file_path, "w") as tmp_file:
+        # aqui obtenho a data de última modificação formatada como string
+        last_modified_date = datetime.fromtimestamp(
+            os.path.getmtime(tmp_file_path)
+        ).strftime("%Y-%m-%d")
+        tmp_file.write(
+            f"File name: my_file.txt\n"
+            f"File size in bytes: {os.path.getsize(tmp_file_path)}\n"
+            "File type: file\n"
+            "File extension: .txt\n"
+            f"Last modified date: {last_modified_date}\n"
+        )
+    # chama a função a passa como padrão o caminho do arquivo temporário
+    show_details({"base_path": str(tmp_file_path)})
+
+    # captura a saída padrão
+    captured = capsys.readouterr()
+    expected_output = (
+        f"File name: my_file.txt\n"
+        f"File size in bytes: {os.path.getsize(tmp_file_path)}\n"
+        "File type: file\n"
+        "File extension: .txt\n"
+        f"Last modified date: {last_modified_date}\n"
+    )
+    assert captured.out == expected_output
+
+
+# test para caso de arquivo sem extensão
+def test_show_details_no_extension(capsys, tmp_path):
+    tmp_file_path = tmp_path / "no_extension"
+    with open(tmp_file_path, "w") as tmp_file:
+        last_modified_date = datetime.fromtimestamp(
+            os.path.getmtime(tmp_file_path)
+        ).strftime("%Y-%m-%d")
+        tmp_file.write(
+            f"File name: no_extension\n"
+            f"File size in bytes: {os.path.getsize(tmp_file_path)}\n"
+            "File type: file\n"
+            "File extension: [no extension]\n"
+            f"Last modified date: {last_modified_date}\n"
+        )
+    show_details({"base_path": str(tmp_file_path)})
+
+    captured = capsys.readouterr()
+    expected_output = (
+        f"File name: no_extension\n"
+        f"File size in bytes: {os.path.getsize(tmp_file_path)}\n"
+        "File type: file\n"
+        "File extension: [no extension]\n"
+        f"Last modified date: {last_modified_date}\n"
+    )
+    assert captured.out == expected_output
--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,4 +1,4 @@
-from pro_filer.actions.main_actions import show_preview  # NOQA
+from pro_filer.actions.main_actions import show_preview
 import pytest
 
 
@@ -11,21 +11,53 @@ import pytest
         ),
         (
             {
-                "all_files": [
-                    "src/__init__.py",
-                    "src/app.py",
-                ],
+                "all_files": ["src/__init__.py", "src/app.py"],
                 "all_dirs": ["src", "src/utils"],
             },
             "Found 2 files and 2 directories\n"
             "First 5 files: ['src/__init__.py', 'src/app.py']\n"
             "First 5 directories: ['src', 'src/utils']\n",
         ),
+        (
+            {
+                "all_files": [
+                    "src/__init__.py",
+                    "src/app.py",
+                    "src/utils/f1.py",
+                    "src/utils/f2.py",
+                    "src/utils/f3.py",
+                    "src/utils/f4.py",
+                    "src/utils/f5.py",
+                    "src/utils/f6.py",
+                ],
+                "all_dirs": ["src", "src/utils"],
+            },
+            "Found 8 files and 2 directories\n"
+            "First 5 files: ['src/__init__.py', 'src/app.py', "
+            "'src/utils/f1.py', 'src/utils/f2.py', 'src/utils/f3.py']\n"
+            "First 5 directories: ['src', 'src/utils']\n",
+        ),
+        (
+            {
+                "all_files": ["src/__init__.py", "src/app.py"],
+                "all_dirs": [
+                    "src/utils/dir1",
+                    "src/utils/dir2",
+                    "src/utils/dir3",
+                    "src/utils/dir4",
+                    "src/utils/dir5",
+                ],
+            },
+            "Found 2 files and 5 directories\n"
+            "First 5 files: ['src/__init__.py', 'src/app.py']\n"
+            "First 5 directories: ['src/utils/dir1', 'src/utils/dir2', "
+            "'src/utils/dir3', 'src/utils/dir4', 'src/utils/dir5']\n",
+        ),
     ],
 )
 def test_show_preview(capsys, context, expected_result):
-    # o capsys vai capturar a saída padrão stdout
+    # O capsys vai capturar a saída padrão stdout
     show_preview(context)
-    
+
     captured = capsys.readouterr()
     assert captured.out == expected_result
