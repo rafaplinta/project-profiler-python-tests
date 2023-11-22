0f937d0036da44d65e44d39ab4e525acf6fb50bc
Rafaela Plinta <ra.plinta@gmail.com>
2023-11-22 08:58:22 -0300

    Fix show_deepest_file

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,16 @@
 """Arquivo que estudantes devem editar"""
 
 
+# essa função recebe o caminho de parâmetro e conta os /
+def count_slashes(path):
+    return path.count("/")
+
+
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=count_slashes)
         print(f"Deepest file: {deepest_file}")
 
 
@@ -26,3 +31,16 @@ def find_file_by_name(context, search_term, case_sensitive=True):
             found_files.append(path)
 
     return found_files
+
+
+# Exemplo 1:
+context1 = {
+    "all_files": [
+        "/home/trybe/Downloads/trybe_logo_pequena.png",
+        "/home/trybe/Documents/aula/python/tests.txt",
+    ]
+}
+
+show_deepest_file(context1)
+# Saída esperada:
+# Deepest file: /home/trybe/Documents/aula/python/tests.txt
