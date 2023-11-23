3899da8dc7fe7024edaa1e2b5eedc78259a012b2
Rafaela Plinta <ra.plinta@gmail.com>
2023-11-23 16:17:19 -0300

    Fix - Tests show_disk_usage

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,81 +1,87 @@
-# from pro_filer.actions.main_actions import show_disk_usage
-# import os
+from pro_filer.actions.main_actions import show_disk_usage
+import os
+import math
 
 
-# # Teste função com parâmetro vazio
-# def test_show_disk_usage_no_file(capsys):
-#     mock = {"all_files": []}
-#     show_disk_usage(mock)
-#     captured = capsys.readouterr()
-#     assert captured.out == "Total size: 0\n"
+def test_show_disk_usage_no_file(capsys):
+    mock = {"all_files": []}
+    show_disk_usage(mock)
+    captured = capsys.readouterr()
+    assert captured.out == "Total size: 0\n"
 
 
-# # função que cria arquivo
-# def write_file(tmp_path, file_name, content=""):
-#     file_path = tmp_path / file_name
-#     with open(file_path, "w") as file:
-#         file.write(content)
-#     return file_path
+def test_show_disk_usage_calculates_total_space_correctly(
+    tmp_path, capsys, monkeypatch
+):
+    # crio os paths
+    file1 = tmp_path / "file1.txt"
+    file2 = tmp_path / "file2.txt"
 
+    # escrevo conteúdo nos meus arquivos temporários
+    file1.write_text("Meu arquivo")
+    file2.write_text("Conteúdo do arquivo 2")
 
-# def test_show_details_all_info(capsys, tmp_path, monkeypatch):
-#     # passo o path e crio os arquivos temporários
-#     # isso me retornará objetos do tipo PosixPath
-#     file_1_path = write_file(tmp_path, "my_app.py", "Conteúdo do meu arquivo")
-#     file_2_path = write_file(tmp_path, "new_file.py")
+    # converto eles de PosixPath para string
+    file_1_path_str = str(file1)
+    file_2_path_str = str(file2)
 
-#     # aqui eu transformo os objetdos gerados do tipo PosixPath em strings
-#     # para que a função get_printable_file_path os aceite
-#     file_1_path_str = str(file_1_path)
-#     file_2_path_str = str(file_2_path)
+    context = {"all_files": [file_1_path_str, file_2_path_str]}
 
-#     # crio o dicionário que será passado como parâmetro
-#     context = {"all_files": [file_1_path_str, file_2_path_str]}
+    total_size = os.path.getsize(file1) + os.path.getsize(file2)
 
-#     # aqui obtenho o tamanho total dos arquivos recebidos como parâmetro
-#     total_size = os.path.getsize(file_1_path) + os.path.getsize(file_2_path)
+    # Mock função _get_printable_file_path
+    def mock_get_printable_file_path(file_path):
+        file_path_str = str(file_path)
+        if len(file_path_str) > 60:
+            return file_path_str[:27] + "..." + file_path_str[-30:]
+        else:
+            return file_path_str + " " * (60 - len(file_path_str))
 
-#     # Mock função _get_printable_file_path
-#     def mock_get_printable_file_path(file_path):
-#         file_path_str = str(file_path)
-#         if len(file_path_str) > 60:
-#             return file_path_str[:27] + "..." + file_path_str[-30:]
-#         else:
-#             return file_path_str
+    monkeypatch.setattr(
+        "pro_filer.cli_helpers._get_printable_file_path",
+        mock_get_printable_file_path,
+    )
 
-#     # ordena os arquivos com base no tamanho
-#     sorted_files = sorted(
-#         context["all_files"],
-#         key=lambda file_path: os.path.getsize(file_path),
-#         reverse=True,
-#     )
+    # instancio minha função com o contexto criado
+    show_disk_usage(context)
+    # capturo saída padrão com o capsys
+    captured = capsys.readouterr()
 
-#     # substitui a lista original pela versão ordenada
-#     context["all_files"] = sorted_files
+    # aqui eu calculo a porcentagem e arrendondo para baixo com o math.floor
+    file1_percentage = math.floor((os.path.getsize(file1) / total_size) * 100)
+    file2_percentage = math.floor((os.path.getsize(file2) / total_size) * 100)
 
-#     # chamo a função mockada no lugar da real usando o monkeypatch
-#     monkeypatch.setattr(
-#         "pro_filer.cli_helpers._get_printable_file_path",
-#         mock_get_printable_file_path,
-#     )
+    # crio uma tupla com o path do arquivo e o seu tamanho,
+    # pela qual vou iterar para encontrar a maior e a menor
+    # percentagem
+    file_percentages = [(file1, file1_percentage), (file2, file2_percentage)]
 
-#     # instancio minha função
-#     show_disk_usage(context)
-#     # leio a saída padrão com o capsys
-#     captured = capsys.readouterr()
+    # função para extrair a percentagem (index 1), de cada tupla
+    def get_percentage(tuple_element):
+        return tuple_element[1]
 
-#     expect_result = (
-#         f"'{mock_get_printable_file_path(file_1_path)}': "
-#         + f"{os.path.getsize(file_1_path)} (100%)\n"
-#         + f"'{mock_get_printable_file_path(file_2_path)}': "
-#         + f"{os.path.getsize(file_2_path)} (0%)\n"
-#         + f"Total size: {total_size}\n"
-#     )
+    # encontra o maior elemento
+    largest_file, largest_percentage = max(
+        file_percentages, key=get_percentage
+    )
 
-#     # verifica se todas as linhas da saída esperada estão presentes na saída
-#     assert all(
-#         # remove espaços em branco e verifica se a presença da saida
-#         line.strip() in captured.out.replace(" ", "").strip()
-#         # itera sobre cada linha da saída esperada
-#         for line in expect_result.replace(" ", "").strip().split("\n")
-#     )
+    # encontra o menor elemento
+    smallest_file, smallest_percentage = min(
+        file_percentages, key=get_percentage
+    )
+    # O método format() é usado para substituir os espaços reservados {}
+    # em uma string pelos valores fornecidos como argumentos para o método.
+    # https://www.w3schools.com/python/ref_string_format.asp
+    expect_result = "'{}': {} ({}%)\n'{}': {} ({}%)\nTotal size: {}\n".format(
+        mock_get_printable_file_path(largest_file),
+        os.path.getsize(largest_file),
+        largest_percentage,
+        mock_get_printable_file_path(smallest_file),
+        os.path.getsize(smallest_file),
+        smallest_percentage,
+        total_size,
+    )
+
+    # verifica se a saída capturada é igual à saída esperada,
+    # ignorando espaços em branco
+    assert captured.out.replace(" ", "") == expect_result.replace(" ", "")
